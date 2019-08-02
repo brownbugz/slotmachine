99e1eb4d3596b0681147ca033301df10bf76e18f
sonniakemmer <brownbugz@gmail.com>
2019-08-02 14:14:43 -0700

    Update 8-2-2019 215pm

---
+++ js/main.js
@@ -0,0 +1,41 @@
+// initialize the game
+// pushing button to spin starts the game / animates the reel
+
+// there should be three columns for reels
+
+// inside the reels are different photos/squares
+// photos need to match in three
+
+// only one line three-image-combination will win something
+
+// below will be an array of winning combinations
+// with matching dollar wins
+// lucky 7 image wins $10
+// cherry image wins $50
+// bar image wins $25
+// jackpot image wins $10
+
+/* 
+
+const winCombos = {
+    lucky7: 
+    cherry:
+    bar:
+    jackpot:
+
+}
+
+*/
+
+// there should be a starting $50 amount for the player to use
+// dollar amount will decrease upon spin
+// dollar amount will increase upon win
+
+// images randomly lands on the middle of the reel
+// win combination to be checked
+
+
+// every push of the button spends $5 
+// the game resets when there's no more dollars to spend
+
+

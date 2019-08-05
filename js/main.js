7d0c326f326e377c7ee242134ff7df789552edd5
sonniakemmer <brownbugz@gmail.com>
2019-08-05 08:38:34 -0700

    loaded images

--- js/main.js
+++ js/main.js
@@ -1,44 +1,71 @@
-// const variables
-const IMAGES = [
+// author @brownbugz - Sonnia Kemmer
 
+// const variables
+const  = {
+    'seven':
+    'cherry':
+    'bell':
+    'orange':
+    'watermelon':
+    'grapes':
+    'apple':
+}
+
+const SLOTS = {
+    'seven': 'slots-images/seven.png',
+    'cherry': 'slots-images/cherry.png',
+    'bell': 'slots-inages/bell.png',
+    'orange': 'slots-images/orange.png',
+    'watermelon': 'slots-images/watermelon.png',
+    'grapes': 'slots-images/grapes.png',
+    'apple': 'slots-images/apple.png'
+}
+
+const winCombos = [
+    ['seven', 'seven', 'seven'],
+    ['cherry', 'cherry', 'cherry'],
+    ['bell', 'bell', 'bell'],
+    ['orange', 'orange', 'orange'],
+    ['watermelon', 'watermelon', 'watermelon'],
+    ['grapes', 'grapes', 'grapes'],
+    ['apple', 'apple', 'apple']
 ]
 
-// initialize the game
 
-// pushing button to spin starts the game / animates the reel
+/*----- app's state (variables) -----*/ 
+let board, player, wins, losses;
+
+/*----- cached element references -----*/ 
+
+
+/*----- event listeners -----*/ 
+document.querySelector().addEventListener('click', handleClick);
+
+/*----- functions -----*/
+
+// initialize the game
+init();
 
 // there should be three columns for reels
-
 // inside the reels are different photos/squares
 // photos need to match in three
 
-// only one line three-image-combination will win something
+function init() {
+    board = [null, null, null];
+    player = null;
+    render();
+}
 
-// below will be an array of winning combinations
-// with matching dollar wins
-// lucky 7 image wins $10
-// cherry image wins $50
-// bar image wins $25
-// jackpot image wins $10
-
-
-/* 
-
-const winCombos = {
-    lucky7: 
-    cherry:
-    bar:
-    jackpot:
+// pushing button to spin starts the game / animates the reel
+function render() {
 
 }
 
-*/
 
 // there should be a starting $50 amount for the player to use
 // dollar amount will decrease upon spin
 // dollar amount will increase upon win
 
-// images randomly lands on the middle of the reel
 // win combination to be checked
 
 

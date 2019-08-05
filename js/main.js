66d2d481350b954fe1516b768ccfaad38935497c
sonniakemmer <brownbugz@gmail.com>
2019-08-05 13:47:35 -0700

    saving before replacing some code

--- js/main.js
+++ js/main.js
@@ -3,24 +3,24 @@
 // variables
 var SLOTS = {
     'null': 'slotmachine/slots-images/bigwin.jpg',
-    // '0': 'slotmachine/slots-images/seven.png',
-    // '1': 'slotmachine/slots-images/cherry.png',
-    // '2': 'slotmachine/slots-images/bell.png',
-    // '3': 'slotmachine/slots-images/orange.png',
-    // '4': 'slotmachine/slots-images/watermelon.png',
-    // '5': 'slotmachine/slots-images/grapes.png',
-    // '6': 'slotmachine/slots-images/apple.png'
+    '0': 'slotmachine/slots-images/seven.png',
+    '1': 'slotmachine/slots-images/cherry.png',
+    '2': 'slotmachine/slots-images/bell.png',
+    '3': 'slotmachine/slots-images/orange.png',
+    '4': 'slotmachine/slots-images/watermelon.png',
+    '5': 'slotmachine/slots-images/grapes.png',
+    '6': 'slotmachine/slots-images/apple.png'
 }
 
 var IMAGES = {
     'null': 'bigwin',
-    // '0': 'seven',
-    // '1': 'cherry',
-    // '2': 'bell',
-    // '3': 'orange',
-    // '4': 'watermelon',
-    // '5': 'grapes',
-    // '6': 'apple'
+    '0': 'seven',
+    '1': 'cherry',
+    '2': 'bell',
+    '3': 'orange',
+    '4': 'watermelon',
+    '5': 'grapes',
+    '6': 'apple'
 }
 
 
@@ -34,19 +34,18 @@ const winCombos = [
     ['6', '6', '6']
 ]
 
-let board, reels, wins, losses;
+var board, reels, wins, losses; 
 
 /*----- cached element references -----*/ 
-// let squares = document.querySelector('#box', idx);
+let squares = document.querySelector('#box');
 
 /*----- event listeners -----*/ 
-// document.querySelector('#spinner').addEventListener('click', spnBtn);
+document.querySelector('#spinner').addEventListener('click', spnBtn);
 
 /*----- functions -----*/
 
 // initialize the game
 init();
-
 // there should be three columns and one row for reels
 // inside the reels are different photos/squares
 // photos need to match in three
@@ -54,24 +53,31 @@ init();
 function init() {
     board = [null, null, null];
 
-    console.log("start");
     render();
 }
 
 function render() {
     console.log("do you render?");
 
-//     board.forEach(function(reel, idx) {
-//         document.getElementById("photo"+idx).src = SLOTS[reel];
-//     });
+    board.forEach(function(reel, idx) {
+        document.getElementById("photo"+idx).src = SLOTS[reel];
+    });
+}
+    // if (sqrLeft === null && sqrMiddle === null && sqrRight === null) {
+    //     document.getElementById("photo"+idx).src = SLOTS[reel];
+    //     console.log('start');
+
 
 //     if (winCombos === )
 // }
 
 // // pushing button to spin starts the game / animates the reel
-// function spinBtn(evt) {
+function spinBtn(evt) {
+    console.log('start spin');
 
-// }
+    let idx = parseInt(evt.target.id.replace('photo', ''));
+
+}
 
 // there should be a starting $50 amount for the player to use
 // dollar amount will decrease upon spin
@@ -82,5 +88,3 @@ function render() {
 
 // every push of the button spends $5 
 // the game resets when there's no more dollars to spend
-
-

231bdec8c32fbfbaf9e692c16e0c13b0b8b0ae27
sonniakemmer <brownbugz@gmail.com>
2019-08-06 16:58:53 -0700

    spinEffect works

--- js/main.js
+++ js/main.js
@@ -6,11 +6,42 @@ const spinCost = 5;
 
 const startImage = "images/bigwin.jpg";
 
-const reels = [0, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 2, 3, 4, 5, 6, 3, 4, 5, 6, 4, 5, 6, 5, 6, 6];
+const reels = [
+    0, 1, 2, 3, 4, 5, 6, 
+    1, 2, 3, 4, 5, 6, 
+    2, 3, 4, 5, 6, 
+    3, 4, 5, 6, 
+    4, 5, 6, 
+    5, 6, 
+    6, 
+    6, 
+    6, 5, 
+    6, 5, 4, 
+    6, 5, 4, 3, 
+    6, 5, 4, 3, 2, 
+    6, 5, 4, 3, 2, 1, 
+    6, 5, 4, 3, 2, 1, 0
+];
 
-const payouts = [150, 120, 90, 60, 30, 15, 5];
+const payouts = [
+    150, //0
+    120, //1
+    90,  //2
+    60,  //3
+    30,  //4
+    15,  //5
+    5    //6
+];
 
-const grapes = 6;
+const winCombos = [
+    [0],
+    [1],
+    [2],
+    [3],
+    [4],
+    [5],
+    [6]
+];
 
 // variables
 var startCredits = 50;
@@ -32,13 +63,12 @@ var reel0, reel1, reel2;
 
 var maxValue = reels.length;
 
-
 /*----- functions -----*/
 
 // initialize the game
 
 function reset() {
-    console.log("reset begins");
+    // console.log("reset begins");
     wins = 0;
     startCredits = 50;
     resetReels();
@@ -46,8 +76,7 @@ function reset() {
 
     //change the reel images to startImage
 function resetReels() {
-    console.log('resetReels'); 
-    // to-do: make it to a for loop
+    // console.log('resetReels'); 
     document.getElementById('photo0').src = startImage;
     document.getElementById('photo1').src = startImage;
     document.getElementById('photo2').src = startImage;
@@ -62,22 +91,50 @@ function randomNum() {
 
 // // pushing button to spin starts the game / animates the reel
 function spinBtn() {
-    console.log('this spins the reels');
-    //to-do: write a for loop
+    // console.log('this spins the reels');
    reel0 = randomNum();
    reel1 = randomNum();
    reel2 = randomNum();
 }
 
 function changePhotos() {
+    // console.log('this changes photos');
     document.getElementById('photo0').src = PHOTOS[reel0];
     document.getElementById('photo1').src = PHOTOS[reel1];
     document.getElementById('photo2').src = PHOTOS[reel2];
 }
 
 function spinEffect() {
-
+    let x = setInterval(function() {
+        spinBtn();
+        changePhotos();
+        // console.log('Hello!');
+    }, 30);
+    
+    setTimeout(function() {
+        clearInterval(x);
+    }, 200);
 }
+
+function earnings() {
+    console.log('this works')
+
+    // if all reels are the same, i want to associate the reels number to payouts index value
+    // i want the reels number associated to the photo value also
+    // then i can get the number to add to whatever is the amount of credits
+}
+    
+
+// function spinDebits() {
+//     var y = document.getElementById('#spinner');
+//     if (spinner.clicked == true) {
+//         wins = startCredits - spinCost;
+//     } else {
+//         wins = 0;
+//     };
+// }
+// console.log(spinDebits);
+    // document.getElementById('#bank').innerHTML(`Credits: + ${spinDebits}`);
 
 
 // there should be a starting $50 amount for the player to use

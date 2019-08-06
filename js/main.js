6930aad22aa01fec766e724150722093330c918f
sonniakemmer <brownbugz@gmail.com>
2019-08-05 22:23:21 -0700

    added functions spin-reset-randomnum

--- js/main.js
+++ js/main.js
@@ -1,68 +1,75 @@
 // author @brownbugz - Sonnia Kemmer
+//console.log('in javascript');
+
+var startCredits = 50;
+
+const spinCost = 5;
+
+const startImage = "images/bigwin.jpg";
 
 // variables
-var SLOTS = {
-    'null': 'slotmachine/slots-images/bigwin.jpg',
-    '0': 'slotmachine/slots-images/seven.png',
-    '1': 'slotmachine/slots-images/cherry.png',
-    '2': 'slotmachine/slots-images/bell.png',
-    '3': 'slotmachine/slots-images/orange.png',
-    '4': 'slotmachine/slots-images/watermelon.png',
-    '5': 'slotmachine/slots-images/grapes.png',
-    '6': 'slotmachine/slots-images/apple.png'
-}
+var PHOTOS = [
+    "images/seven.png",         //0
+    "images/apple.png",         //1
+    "images/cherry.png",        //2
+    "images/bell.png",          //3
+    "images/orange.png",        //4
+    "images/watermelon.png",    //5
+    "images/grapes.png"         //6
+];
 
-var IMAGES = {
-    'null': 'bigwin',
-    '0': 'seven',
-    '1': 'cherry',
-    '2': 'bell',
-    '3': 'orange',
-    '4': 'watermelon',
-    '5': 'grapes',
-    '6': 'apple'
-}
+var wins = 0;
 
+const reels = [0, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 2, 3, 4, 5, 6, 3, 4, 5, 6, 4, 5, 6, 5, 6, 6];
 
-const winCombos = [
-    ['0', '0', '0'],
-    ['1', '1', '1'],
-    ['2', '2', '2'],
-    ['3', '3', '3'],
-    ['4', '4', '4'],
-    ['5', '5', '5'],
-    ['6', '6', '6']
-]
+var reel0, reel1, reel2;
 
-var board, reels, wins, losses; 
+var maxValue = reels.length;
 
-/*----- cached element references -----*/ 
-let squares = document.querySelector('#box');
-
-/*----- event listeners -----*/ 
-document.querySelector('#spinner').addEventListener('click', spnBtn);
+// /*----- cached element references -----*/ 
 
 /*----- functions -----*/
 
 // initialize the game
-init();
-// there should be three columns and one row for reels
-// inside the reels are different photos/squares
-// photos need to match in three
 
-function init() {
-    board = [null, null, null];
-
-    render();
+function reset() {
+    console.log("reset begins");
+    wins = 0;
+    startCredits = 50;
+    resetReels();
+    //change the reel images to bigwin
 }
 
-function render() {
-    console.log("do you render?");
-
-    board.forEach(function(reel, idx) {
-        document.getElementById("photo"+idx).src = SLOTS[reel];
-    });
+function resetReels() {
+    console.log('resetReels'); 
+    // to-do: make it to a for loop
+    document.getElementById('photo0').src = startImage;
+    document.getElementById('photo1').src = startImage;
+    document.getElementById('photo2').src = startImage;
 }
+
+function randomNum() {
+    let reelValue = Math.floor(Math.random() * maxValue);
+    reelValue = reels[reelValue];
+    console.log(reelValue);
+    return reelValue;
+}
+
+function spinBtn() {
+    console.log('this spins the reels');
+    //to-do: write a for loop
+   reel0 = randomNum();
+   reel1 = randomNum();
+   reel2 = randomNum();
+}
+
+function changePhotos() {
+
+
+}
+
+
+// }
     // if (sqrLeft === null && sqrMiddle === null && sqrRight === null) {
     //     document.getElementById("photo"+idx).src = SLOTS[reel];
     //     console.log('start');
@@ -72,12 +79,9 @@ function render() {
 // }
 
 // // pushing button to spin starts the game / animates the reel
-function spinBtn(evt) {
-    console.log('start spin');
 
-    let idx = parseInt(evt.target.id.replace('photo', ''));
 
-}
+// }
 
 // there should be a starting $50 amount for the player to use
 // dollar amount will decrease upon spin
@@ -88,3 +92,5 @@ function spinBtn(evt) {
 
 // every push of the button spends $5 
 // the game resets when there's no more dollars to spend
+
+

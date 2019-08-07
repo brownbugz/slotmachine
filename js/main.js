e5d222557834e80e371dd1b8bb5b5aac897c0034
sonniakemmer <brownbugz@gmail.com>
2019-08-07 11:01:56 -0700

    slot machine works -- trying to add audio and cleanup code

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 // author @brownbugz - Sonnia Kemmer 2019 (C)
-//console.log('in javascript');
 
-// const variables
 const spinCost = 5;
 
 const startImage = "images/bigwin.jpg";
@@ -33,7 +31,11 @@ const payouts = [
     5     //6
 ];
 
-// variables
+const spinAudio = "audio/spin.mp3";
+
+const winAudio = "audio/loudwin.mp3";
+
+
 var bankTotal = 50;
 
 var PHOTOS = [
@@ -54,123 +56,79 @@ var reel0, reel1, reel2;
 
 var maxValue = reels.length;
 
-/*----- functions -----*/
-
-// initialize the game
-
 function reset() {
-    // console.log("reset begins");
     wins = 0;
     bankTotal = 50;
     resetReels();
     showBankTotal();
 }
 
-    //change the reel images to startImage
 function resetReels() {
-    // console.log('resetReels'); 
     document.getElementById('photo0').src = startImage;
     document.getElementById('photo1').src = startImage;
     document.getElementById('photo2').src = startImage;
-
 }
 
 function randomNum() {
     let reelValue = Math.floor(Math.random() * maxValue);
-    reelValue = reels[reelValue];
-    console.log(reelValue);
-    return reelValue;
+        reelValue = reels[reelValue];
+        return reelValue;
 }
 
-// // pushing button to spin starts the game / animates the reel
 function spinBtn() {
-    // console.log('this spins the reels');
     if (bankTotal > 0) {
-   reel0 = randomNum();
-   reel1 = randomNum();
-   reel2 = randomNum();
-   changePhotos();
-//    spinEffect();
-   earnings(reel0, reel1, reel2);
-   showBankTotal();
-   showMeMyWins();
-   spinDeducts();
+        reel0 = randomNum();
+        reel1 = randomNum();
+        reel2 = randomNum();
+        changePhotos();
+        earnings(reel0, reel1, reel2);
+        showBankTotal();
+        showMeMyWins();
+        spinDeducts();
+        playSpinAudio();
     } else {
         reset();
     }
 }
 
 function changePhotos() {
-    // console.log('this changes photos');
     document.getElementById('photo0').src = PHOTOS[reel0];
     document.getElementById('photo1').src = PHOTOS[reel1];
     document.getElementById('photo2').src = PHOTOS[reel2];
 }
 
-// function spinEffect() {
-//     let x = setInterval(function() {
-
-//         // console.log('Hello!');
-//     }, 30);
-    
-//     setTimeout(function() {
-//         clearInterval(x);
-//     }, 200);
-// }
-
 function earnings(value1, value2, value3) {
-    console.log('earnings() begins');
-    console.log(value1);
-    console.log(value2);
-    console.log(value3);
-
     if (value1 == value2 && value2 == value3) {
         pays = payouts[value1];
         bankTotal = bankTotal + pays;
-        console.log('this is pays');
-        console.log(pays);
     }
     showBankTotal();
+    playWinAudio();
 }
     
 function showBankTotal() {
-    console.log('deposits begins');
-    console.log(bankTotal);
     document.getElementById('bank').innerHTML = ("Credits: " + bankTotal);
 }
 
-
-
-    // if all reels are the same, i want to associate the reels number to payouts index value
-    // i want the reels number associated to the photo value also
-    // then i can get the number to add to whatever is the amount of credits
-
-    
-
 function spinDeducts() {
-    console.log('give me your money');
     bankTotal = bankTotal - spinCost;
-    console.log(bankTotal);
     document.getElementById('bank').innerHTML = ("Credits: " + bankTotal);
 }
 
 function showMeMyWins(){
-    console.log('i won something');
-
     document.getElementById('winnings').innerHTML = ("Won: " + pays);
+}
+
+function playSpinAudio() {
+    console.log('this play when i spin');
 
 }
 
+function playWinAudio() {
+    console.log('this play when i win something');
 
-
-// there should be a starting $50 amount for the player to use
-// dollar amount will decrease upon spin
-// dollar amount will increase upon win
-
-// win combination to be checked
-
-
-// every push of the button spends $5 
-// the game resets when there's no more dollars to spend
-
+    if (wins > 0) {
+        
+    }
+}
 
